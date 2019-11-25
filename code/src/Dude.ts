443a6c25b28a9cd653ad4254cd790b41aafefda0
Nico Hauser <me@tyratox.ch>
2019-11-25 17:39:58 +0100

    Added normal distribution

--- code/src/Dude.ts
+++ code/src/Dude.ts
@@ -1,9 +1,7 @@
 import { getBody } from "./index";
+import randomNormal from "random-normal";
 
-const MAX_VELOCITY = 150;
-const MAX_ACCELERATION = 50;
-const MAX_RADIUS = 9;
-const MAX_VISUAL_RANGE = 50;
+import { CONSTANTS } from "./controls";
 
 class Dude extends Phaser.GameObjects.Arc {
   maxVelocity: number;
@@ -18,32 +16,89 @@ class Dude extends Phaser.GameObjects.Arc {
   fitness: number;
   weight: number;
   age: number;
+
+  //the currently tracked sign
   sign: { x: number; y: number };
 
-  constructor(
-    x: number,
-    y: number,
-    fitness: number,
-    weight: number,
-    age: number,
-    name: string,
-    scene: Phaser.Scene
-  ) {
-    const radius = Math.min(MAX_RADIUS, MAX_RADIUS * (weight / fitness));
+  constructor(x: number, y: number, name: string, scene: Phaser.Scene) {
+    const weight = Math.min(
+      Math.max(
+        randomNormal({
+          mean: CONSTANTS.MEAN_DUDE_WEIGHT,
+          dev: CONSTANTS.DUDE_WEIGHT_STD_DEV
+        }),
+        0.1
+      ),
+      1
+    );
+    const fitness = Math.min(
+      Math.max(
+        randomNormal({
+          mean: CONSTANTS.MEAN_DUDE_FITNESS,
+          dev: CONSTANTS.DUDE_FITNESS_STD_DEV
+        }),
+        0.1
+      ),
+      1
+    );
+
+    const r = Math.max(
+      randomNormal({
+        mean: CONSTANTS.MEAN_DUDE_RADIUS,
+        dev: CONSTANTS.DUDE_RADIUS_STD_DEV
+      }),
+      8
+    );
+
+    const radius = Math.min(
+      Math.max((r * Math.pow(weight, 2)) / fitness, 9),
+      18
+    );
     super(scene, x, y, radius, 0, 360, true, 0xf1c40f, 1);
 
     scene.children.add(this);
 
     this.sign = { x: 0, y: 0 };
-    this.fitness = fitness; // Math.random(),
-    this.weight = weight; // 0.6 + Math.random() * 0.4
-    this.age = age; // Math.random()
+    this.fitness = fitness;
+    this.weight = weight;
+    this.age = Math.min(
+      Math.max(
+        randomNormal({
+          mean: CONSTANTS.MEAN_DUDE_AGE,
+          dev: CONSTANTS.DUDE_AGE_STD_DEV
+        }),
+        0.1
+      ),
+      1
+    );
 
-    this.maxVelocity = MAX_VELOCITY * ((fitness * weight) / age);
-    this.maxAcceleration = MAX_ACCELERATION * ((fitness * weight) / age);
+    this.maxVelocity = Math.max(
+      randomNormal({
+        mean: CONSTANTS.MEAN_DUDE_MAX_VELOCITY,
+        dev: CONSTANTS.DUDE_MAX_VELOCITY_STD_DEV
+      }) *
+        ((fitness * weight) / this.age),
+      100
+    );
+    this.maxAcceleration =
+      Math.max(
+        randomNormal({
+          mean: CONSTANTS.MEAN_DUDE_MAX_ACCELERATION,
+          dev: CONSTANTS.DUDE_MAX_ACCELERATION_STD_DEV
+        }),
+        30
+      ) *
+      ((fitness * weight) / this.age);
     this.radius = radius;
     this.stressLevel = Math.random();
-    this.visualRange = MAX_VISUAL_RANGE / age;
+    this.visualRange =
+      Math.max(
+        randomNormal({
+          mean: CONSTANTS.MEAN_DUDE_VISUAL_RANGE,
+          dev: CONSTANTS.DUDE_VISUAL_RANGE_STD_DEV
+        }),
+        300
+      ) / this.age;
     this.health = 4000;
     this.name = name;
 
